ae42d33f1e634cb43724453d15a7a9e9519dba46
bgagan911 <bgagan911@users.noreply.github.com>
2018-11-26 01:44:39 +0530

    Update and rename domain-enumeration to domain-enumeration.sh

--- domain-enumeration.sh
+++ domain-enumeration.sh
@@ -1,1 +1,3 @@
+# Source: https://twitter.com/JaneScott_/status/1065995260554170369
+
 curl "https://crt.sh/?q=%.example.com&output=json" | jq '.name_value' | sed 's/\"//g' | sed 's/\*\.//g' | sort -u
